51ab9cdcd05f9646c49416248c898e3a225345d3
ViniSPG <vspigiorin@gmail.com>
2024-06-19 19:23:15 -0300

    criado script para executar com o cron

--- start_plugin.sh
+++ start_plugin.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cd /path/to/redmine
-/usr/local/bin/bundle exec rake send_feedback_reminders:send RAILS_ENV=production
+cd /usr/src/redmine
+
+export PATH=$PATH:/usr/local/bundle/bin
+
+bundle exec rake send_feedback_reminders:send RAILS_ENV=production
